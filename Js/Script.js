370b2563fbbff039f0b9b73aa2f99c8d23cb6191
MariaFernanda1105 <129109062+MariaFernanda1105@users.noreply.github.com>
2024-08-14 09:39:26 -0300

    Add files via upload

---
+++ Js/Script.js
@@ -0,0 +1,32 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var botaoForm = document.querySelector("#botao-form");
+
+    botaoForm.addEventListener("click", function(event){
+        event.preventDefault();
+       
+        var formAluno = document.querySelector("#form-user");
+
+        // Salvando os valores no localStorage
+        localStorage.setItem('titulo', formAluno.titulo.value);
+        localStorage.setItem('desc', formAluno.desc.value);
+
+        var imagemInput = document.querySelector("#foto-form");
+        
+        // Verifica se há uma imagem selecionada
+        if (imagemInput.files && imagemInput.files[0]) {
+            var fr = new FileReader();
+            
+            fr.onload = function(e) {
+                localStorage.setItem('imagem-form', e.target.result);
+                // Redireciona após carregar a imagem
+                window.location.href = "index.html";
+                window.location.href = "sobremim.html";
+            };
+            
+            fr.readAsDataURL(imagemInput.files[0]);
+        } else {
+            // Redireciona imediatamente se nenhuma imagem for selecionada
+            window.location.href = "index.html";
+        }
+    });
+});
